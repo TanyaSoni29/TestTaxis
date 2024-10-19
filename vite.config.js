ed68ebc645e4800889b4365c80756aad9ba7d3cc
TanyaSoni2912 <tanu139sona@gmail.com>
2024-10-19 15:07:23 +0530

    Vercel config and vite config

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
 	plugins: [react()],
 	rules: [],
+	server: {
+		host: '192.168.1.39', // Replace with your desired IP address
+		port: 3000, // Optional: specify the port as well
+	},
 });
